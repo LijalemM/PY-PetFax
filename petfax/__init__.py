4f7f978ba91533dc21870ce2be1fa5efd1e773ed
Your Name <you@example.com>
2023-04-30 18:02:40 -0700

    database update

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,17 +1,26 @@
 from flask import Flask
+from flask_migrate import Migrate
 
 # App factory function 
 def create_app():
-    app = Flask(__name__)
+      app = Flask(__name__)
 
-   # @app.route('/')
-    #def hello():
-       # return 'Hello, how is Flask going on!?'
+      app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:pg123@localhost:5432/petfax"
+      app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    from . import pet
-    app.register_blueprint(pet.bp)
+      from . import models
+      models.db.init_app(app)
+      migrate = Migrate(app, models.db)
 
-    from . import fact
-    app.register_blueprint(fact.bp)
-    
-    return app 
+      @app.route('/')
+      def index():
+         return 'Hello, how is Flask going on!?'
+
+      from . import pet
+      app.register_blueprint(pet.bp)
+      
+
+      from . import fact
+      app.register_blueprint(fact.bp)
+      
+      return app 
--- petfax/fact.py
+++ petfax/fact.py
@@ -1,11 +1,31 @@
-from flask import Blueprint, render_template, request, redirect
+from flask import (Blueprint, render_template, request, redirect)
+from . import models
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
 
-@bp.route('/', methods = ['GET','POST'])
+@bp.route('/new')
+def new():
+    return render_template('facts/new.html')
+
+@bp.route('/', methods = ['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        print(request.form)
-        return redirect('facts/new.html')
-    
-    return'this is the facts index'
+        if request.method == 'POST':
+            submitter = request.form['submitter']
+            fact = request.form['fact']
+
+            
+        
+
+            new_fact = models.Fact(submitter=submitter, fact=fact)
+            models.db.session.add(new_fact)
+            models.db.session.commit()
+            print(request.form)
+            
+            return redirect('/facts')
+        
+        results = models.Fact.query.all()
+        #for item in results:
+            # print(f"{item.submitter}: {item.fact}")
+
+
+        return render_template('facts/index.html', facts=results)
